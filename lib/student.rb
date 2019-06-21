07f78f9f3a18d90ca0ef795f38aaeb2060e59110
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 16:38:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,13 @@ class Student
   end
 
   def self.drop_table
-
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 end
