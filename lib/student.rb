62211fac234d2130b517a4be4eea28956a7742ff
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 16:51:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-    self.tap{|song| song.new(name, grade).save}
+    #self.tap{|song| song.new(name, grade).save}
   end
 
   def self.create_table
