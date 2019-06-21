32c1f5c6a68b18fd4e6fd74026092489a3f24aa6
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 16:33:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,14 @@
 class Students
   attr_accessor :name, :grade
   attr_reader :id
+  
   def initialize(name, grade, id = nil)
     @name = name
     @grad = grade
     @id = id
+  end
+
+  def self.create_table(name, grade, id)
+
   end
 end
