1496e843271695b5d50866eae6e2a8d969614989
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 16:36:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,13 +18,13 @@ class Students
 
   def self.create_table
     sql =  <<-SQL
-  CREATE TABLE IF NOT EXISTS songs (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    album TEXT
-    )
-    SQL
-DB[:conn].execute(sql)
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
