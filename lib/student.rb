62d710f7ab47482b98bea6a03d206e67d477c3cd
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 16:56:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
     puts self.grade
     sql =  <<-SQL
       INSERT INTO students (name, grade)
-      VALUES (?, ?)
+      VALUES ('jimmy', '11th')
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
   end
